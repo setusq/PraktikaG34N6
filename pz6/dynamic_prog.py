22ef787196b63c41e0cdd2ba730631e28a92c9ff
tsukerin <125801694+setusq@users.noreply.github.com>
2023-10-10 22:40:13 +0300

    Update dynamic_prog.py

--- pz6/dynamic_prog.py
+++ pz6/dynamic_prog.py
@@ -1,1 +1,14 @@
-
+def count_ways(n):
+    if n<=1:
+        return 1
+    d=[0]*(n+1)
+    d[1]=1
+    for i in range(2,n+1):
+        d[i]+=d[i-1]
+        d[i]+=d[i-2]
+        if i%2==0:
+            d[i]+=d[i//2]
+    return d[n]
+n = int(input(('Введите значение n: ')))
+result = count_ways(n)
+print(f'Кол-во способов достичь {n} из точки 1: {result}')
